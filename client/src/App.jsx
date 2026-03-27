ba699619e1d67b7b42eedb663bff2e9acad1f843
Sudhansu-32 <xdsp289@gmail.com>
2026-03-27 22:55:37 +0530

    2nd part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signin from './pages/Signin'
+import Signup from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <h1 className='text-green-600'>Sudhansu Pradhan</h1>
+          <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/sign-in' element={<Signin />} />
+                <Route path='/sign-up' element={<Signup />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+          </BrowserRouter>
   )
 }
