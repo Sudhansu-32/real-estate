fe912a6bfd5b3c1f9bf98dbb65ef58a59803e2ce
Sudhansu-32 <xdsp289@gmail.com>
2026-04-02 14:29:30 +0530

    DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to DB');
+}).catch((err)=>{
+    console.log('error');
+    
+})
 const app = express();
 
 app.listen(3000, ()=>{
